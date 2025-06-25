2087e7a75bd1ebc0dbba735ca1aa9e807f40d88d
_Pm7 <pm7wasnottaken@gmail.com>
2025-06-25 16:22:24 -0500

    Add tangerine pack integration

--- src/main/java/me/pm7/tangerine/listener/JoinListener.java
+++ src/main/java/me/pm7/tangerine/listener/JoinListener.java
@@ -5,18 +5,26 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.UUID;
+
 public class JoinListener implements Listener {
     private static final Tangerine plugin = Tangerine.getPlugin();
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-//        if(plugin.getConfig().getBoolean("automaticallyApplyResourcePack")) {
-//            e.getPlayer().addResourcePack(
-//                    UUID.randomUUID(),
-//                    "",
-//                    new byte[4],
-//                    "the tangerines require this.",
-//                    true
-//            );
-//        }
+        if(plugin.getConfig().getBoolean("automaticallyApplyResourcePack")) {
+            e.getPlayer().addResourcePack(
+                    UUID.randomUUID(),
+                    "https://github.com/Pm7-dev/Tangerine/raw/refs/heads/master/resourcepack.zip",
+                    new byte[] {
+                            (byte) 0xf7, (byte) 0x6b, (byte) 0xa3, (byte) 0x16,
+                            (byte) 0x7e, (byte) 0xc0, (byte) 0xf6, (byte) 0x8a,
+                            (byte) 0xc9, (byte) 0xae, (byte) 0x90, (byte) 0xbe,
+                            (byte) 0x13, (byte) 0x84, (byte) 0x8a, (byte) 0x80,
+                            (byte) 0x3a, (byte) 0x59, (byte) 0x21, (byte) 0x9e
+                    },
+                    "the tangerines require acknowledgement.",
+                    true
+            );
+        }
     }
 }
